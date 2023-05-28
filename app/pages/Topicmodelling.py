f85048e9bb47335d3563eeb8ebaec9eb5d33c589
Eline <eline.westerbeek@gmail.com>
2023-05-28 21:20:19 +0200

    Added topic word clouds now as well :))

--- app/pages/Topicmodelling.py
+++ app/pages/Topicmodelling.py
@@ -2,6 +2,11 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 import os
+from wordcloud import WordCloud
+from nltk.tokenize import RegexpTokenizer
+import nltk.corpus
+from collections import Counter
+import matplotlib.pyplot as plt
 
 st.title("Topic Modelling of The Office (US)")
 
@@ -29,6 +34,7 @@ def load_data():
 df = load_data()
 
 topics = [0, 1, 2, 3, 4, 5, 6, 7, 8, "All"]
+topics_lala = [0, 1, 2, 3, 4, 5, 6, 7, 8,]
 
 
 def show_topics(selected_topic):
@@ -73,5 +79,43 @@ def show_topics(selected_topic):
 selected_topic = st.selectbox(label="Select a topic", options=topics)
 ok = st.button("Generate topics over the seasons")
 
+
 if ok == True:
     show_topics(selected_topic)
+    
+st.title("Wordclouds per topic")
+
+tokenizer = RegexpTokenizer(r'\w+')
+
+with open('most_common_english_words.txt', 'r') as file:
+    most_common = [i.strip('\n') for i in file.readlines()]
+
+stopwords = set(nltk.corpus.stopwords.words('english'))
+
+def topic_wordcloud(selected_topic_lala):
+    topic_lines = df.loc[df['nmf_topic'] == selected_topic_lala, 'line_text_pp']
+    topic_words = []
+    for line in topic_lines:
+        if isinstance(line, str):
+            words = tokenizer.tokenize(line)
+            topic_words.extend(words)
+
+    topic_words = [word.lower() for word in topic_words]
+    topic_words = list(filter(lambda x: x not in most_common, topic_words))
+    topic_words = list(filter(lambda x: x not in stopwords, topic_words))
+    topic_words = list(filter(lambda x: len(x) > 2, topic_words))
+    
+    #convert it to dictionary with values and its occurences
+    word_could_dict = Counter(topic_words)
+    wordcloud = WordCloud(width = 1000, height = 500).generate_from_frequencies(word_could_dict)
+
+    fig = plt.figure(figsize=(15,8))
+    plt.imshow(wordcloud)
+    plt.axis("off")
+    return fig
+
+selected_topic_lala = st.selectbox(label="Select a topic", options=topics_lala)
+lala = st.button("Generate WordCloud")
+
+if lala == True:
+    st.write(topic_wordcloud(selected_topic_lala))
